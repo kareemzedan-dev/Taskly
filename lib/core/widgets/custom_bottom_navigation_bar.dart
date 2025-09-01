88ec1f2e63b76a02d2885de9fa37398f58c7c159
kareemzedan11 <kareemzedan991@gmail.com>
2025-09-01 23:54:02 +0300

    fet: add tabs in client home view

---
+++ lib/core/widgets/custom_bottom_navigation_bar.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:taskly/core/utils/assets_manager.dart';
+import 'package:taskly/core/utils/colors_manger.dart';
+
+class CustomBottomNavigationBar extends StatelessWidget {
+  const CustomBottomNavigationBar({
+    super.key,
+    required this.currentIndex,
+    required this.onTap,
+  });
+
+  final int currentIndex;
+  final ValueChanged<int> onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      backgroundColor: Colors.white,
+      type: BottomNavigationBarType.fixed,
+      currentIndex: currentIndex,
+      onTap: onTap,
+      selectedLabelStyle: const TextStyle(
+        fontSize: 12,
+        fontWeight: FontWeight.bold,
+        color: Colors.transparent,
+      ),
+      unselectedLabelStyle: const TextStyle(
+        fontSize: 12,
+        fontWeight: FontWeight.normal,
+        color: Colors.transparent,
+      ),
+      selectedItemColor: ColorsManager.primary,
+      unselectedItemColor: Colors.black.withOpacity(0.5),
+      items: [
+        BottomNavigationBarItem(
+          icon: Image.asset(
+            Assets.assetsImagesHome4561540,
+            color: Colors.black.withOpacity(0.5),
+            height: 24.h,
+            width: 24.w,
+          ),
+          activeIcon: Image.asset(
+            Assets.assetsImagesHome4561540,
+            color: ColorsManager.primary,
+            height: 24.h,
+            width: 24.w,
+          ),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Image.asset(
+            Assets.assetsImagesJobBoard18989826,
+            color: Colors.black.withOpacity(0.5),
+            height: 24.h,
+            width: 24.w,
+          ),
+          activeIcon: Image.asset(
+            Assets.assetsImagesJobBoard18989826,
+            color: ColorsManager.primary,
+            height: 24.h,
+            width: 24.w,
+          ),
+          label: 'My Jobs',
+        ),
+        BottomNavigationBarItem(
+          icon: Image.asset(
+            Assets.assetsImagesChat6431892,
+            color: Colors.black.withOpacity(0.5),
+            height: 24.h,
+            width: 24.w,
+          ),
+          activeIcon: Image.asset(
+            Assets.assetsImagesChat6431892,
+            color: ColorsManager.primary,
+            height: 24.h,
+            width: 24.w,
+          ),
+          label: 'Messages',
+        ),
+        BottomNavigationBarItem(
+          icon: Image.asset(
+            Assets.assetsImagesUser12366536,
+            color: Colors.black.withOpacity(0.5),
+            height: 24.h,
+            width: 24.w,
+          ),
+          activeIcon: Image.asset(
+            Assets.assetsImagesUser12366536,
+            color: ColorsManager.primary,
+            height: 24.h,
+            width: 24.w,
+          ),
+          label: 'Profile',
+        ),
+      ],
+    );
+  }
+}
--- lib/features/client/presentation/views/client_home_view.dart
+++ lib/features/client/presentation/views/client_home_view.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:taskly/core/utils/assets_manager.dart';
-import 'package:taskly/core/utils/colors_manger.dart';
+import 'package:taskly/core/widgets/custom_bottom_navigation_bar.dart';
 import 'package:taskly/features/client/presentation/views/widgets/client_home_view_body.dart';
 
 class ClientHomeView extends StatefulWidget {
-    ClientHomeView({super.key});
+  const ClientHomeView({super.key});
 
   @override
   State<ClientHomeView> createState() => _ClientHomeViewState();
@@ -16,108 +13,25 @@ class _ClientHomeViewState extends State<ClientHomeView> {
   int currentIndex = 0;
 
   List<Widget> items = [
-      ClientHomeViewBody(),
-      Container(color: Colors.red,),
-      Container(color: Colors.green,),
-      Container(color: Colors.yellow,),
-  
+    const ClientHomeViewBody(),
+    Container(color: Colors.red),
+    Container(color: Colors.green),
+    Container(color: Colors.yellow),
   ];
 
   @override
   Widget build(BuildContext context) {
-    return   Scaffold(
+    return Scaffold(
       backgroundColor: Colors.white,
-      body: SafeArea(child:  items[currentIndex]),
-       
-
-      bottomNavigationBar: CustomBottomNavigationBar(),
+      body: SafeArea(child: items[currentIndex]),
+      bottomNavigationBar: CustomBottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+      ),
     );
   }
- 
-
-  BottomNavigationBar CustomBottomNavigationBar() {
-    return BottomNavigationBar(
-      backgroundColor: Colors.white,
-      type: BottomNavigationBarType.fixed,
-            currentIndex: currentIndex,
-      onTap: (index) {
-     setState(() {
-          currentIndex = index;
-        });
-      },
-      selectedLabelStyle: const TextStyle(
-        fontSize: 12,
-        fontWeight: FontWeight.bold,
-        color: Colors.transparent,
-      ),
-      unselectedLabelStyle: const TextStyle(
-        fontSize: 12,
-        fontWeight: FontWeight.normal,
-        color: Colors.transparent,
-      ),
-      selectedItemColor: ColorsManager.primary,
-      unselectedItemColor: Colors.black.withOpacity(0.5),
-      items: [
-        BottomNavigationBarItem(
-          icon: Image.asset(
-            Assets.assetsImagesHome4561540,
-            color: Colors.black.withOpacity(0.5),
-            height: 24.h,
-            width: 24.w,
-          ),
-          activeIcon: Image.asset(
-            Assets.assetsImagesHome4561540,
-            color: ColorsManager.primary,
-             height: 24.h,
-            width: 24.w,
-          ),
-          label: 'Home',
-        ),
-        BottomNavigationBarItem(
-          icon: Image.asset(
-            Assets.assetsImagesJobBoard18989826,
-            color: Colors.black.withOpacity(0.5),
-             height: 24.h,
-            width: 24.w,
-          ),
-          activeIcon: Image.asset(
-            Assets.assetsImagesJobBoard18989826,
-            color: ColorsManager.primary,
-             height: 24.h,
-            width: 24.w,
-          ),
-          label: 'My Jobs',
-        ),
-        BottomNavigationBarItem(
-          icon: Image.asset(
-            Assets.assetsImagesChat6431892,
-            color: Colors.black.withOpacity(0.5),
-             height: 24.h,
-            width: 24.w,
-          ),
-          activeIcon: Image.asset(
-            Assets.assetsImagesChat6431892,
-            color: ColorsManager.primary,
-             height: 24.h,
-            width: 24.w,
-          ),
-          label: 'Messages',
-        ),
-            BottomNavigationBarItem(
-          icon: Image.asset(
-            Assets.assetsImagesUser12366536,
-            color: Colors.black.withOpacity(0.5),
-             height: 24.h,
-            width: 24.w,
-          ),
-          activeIcon: Image.asset(
-            Assets.assetsImagesUser12366536,
-            color: ColorsManager.primary,
-             height: 24.h,
-            width: 24.w,
-          ),
-          label: 'Profile',
-        ),
-      ],
-    );
-  }}
+}
