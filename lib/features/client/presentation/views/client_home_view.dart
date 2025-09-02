d678b1715a5fb29cd9079dcd9e373893a110d6a6
kareemzedan11 <kareemzedan991@gmail.com>
2025-09-02 12:42:28 +0300

    fet: client profile feature begin

--- lib/features/client/presentation/views/client_home_view.dart
+++ lib/features/client/presentation/views/client_home_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:taskly/core/widgets/custom_bottom_navigation_bar.dart';
 import 'package:taskly/features/client/presentation/views/tabs/home/presentation/views/home_tab_view.dart';
-import 'package:taskly/features/client/presentation/views/tabs/home/presentation/views/widgets/client_home_tab_view_body.dart';
 import 'package:taskly/features/client/presentation/views/tabs/messages/presentation/views/messages_tab_view.dart';
 import 'package:taskly/features/client/presentation/views/tabs/profile/presentation/views/profile_view.dart';
 
--- lib/features/client/presentation/views/tabs/profile/presentation/views/profile_view.dart
+++ lib/features/client/presentation/views/tabs/profile/presentation/views/profile_view.dart
@@ -1,26 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:taskly/core/utils/assets_manager.dart';
 import 'package:taskly/core/widgets/circle_icon_button.dart';
 import 'package:taskly/features/client/presentation/views/tabs/profile/presentation/views/widgets/profile_view_body.dart';
- 
+
 class ProfileViewTab extends StatelessWidget {
   const ProfileViewTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return   Scaffold(
-      appBar:AppBar(
+    return Scaffold(
+      appBar: AppBar(
         backgroundColor: Colors.white,
-        title:   Text('Profile',style: Theme.of(context).textTheme.bodyLarge?.copyWith(fontWeight: FontWeight.w800) ,),
+        title: Text(
+          'Profile',
+          style: Theme.of(
+            context,
+          ).textTheme.bodyLarge?.copyWith(     
+                   fontWeight: FontWeight.w700,
+            fontSize: 20.sp,),
+        ),
         actions: [
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child:   CircularIconButton(image: Assets.assetsImagesSettings),
-        )],
+            child: CircularIconButton(image: Assets.assetsImagesSettings),
+          ),
+        ],
       ),
-   
-      body: ProfileViewBody()
+
+      body: ProfileViewBody(),
     );
   }
 }
