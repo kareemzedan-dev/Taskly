616bfed7ee63bab12d315c405849be0332625ea0
kareemzedan11 <kareemzedan991@gmail.com>
2025-09-03 22:16:05 +0300

    fet: freelancer ui begin

--- lib/main.dart
+++ lib/main.dart
@@ -49,7 +49,7 @@ class Taskly extends StatelessWidget {
 
           onGenerateRoute:
               (settings) => RoutesManager.onGenerateRoute(settings),
-          initialRoute: RoutesManager.jobDetailsView,
+          initialRoute: RoutesManager.freelancerHome,
         );
       },
     );
